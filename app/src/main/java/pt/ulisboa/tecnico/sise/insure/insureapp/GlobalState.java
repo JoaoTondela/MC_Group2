0fb8b1bd5e2ce4597e5753725a83ed706082755a
André Vilaça Moreira <andrevmoreira8@gmail.com>
2018-07-27 20:06:05 +0100

    writing file

--- app/src/main/java/pt/ulisboa/tecnico/sise/insure/insureapp/GlobalState.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/insure/insureapp/GlobalState.java
@@ -1,9 +1,24 @@
 package pt.ulisboa.tecnico.sise.insure.insureapp;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import pt.ulisboa.tecnico.sise.insure.insureapp.datamodel.Customer;
 
 public class GlobalState extends Application {
     private static Integer sessionId;
+    private static Customer customer;
+    String filename = "inSureSavedData";
+    FileInputStream inputStream;
+    FileOutputStream outputStream;
 
     public static Integer getSessionId() {
         return sessionId;
@@ -11,5 +26,47 @@ public class GlobalState extends Application {
 
     public void setSessionId(Integer sessionId){
         this.sessionId = sessionId;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void writeFile(Customer customer) {
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+
+            ObjectOutputStream out = new ObjectOutputStream(outputStream);
+
+            out.writeObject(customer);
+            out.close();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("ListFiles", "FliesWrite: " + fileList()[0]);
+    }
+
+    public Customer readFile() {
+        try {
+            Customer customer = null;
+
+            inputStream = openFileInput(filename);
+            ObjectInputStream in = new ObjectInputStream(inputStream);
+            customer = (Customer) in.readObject();
+            in.close();
+            inputStream.close();
+
+            this.setCustomer(customer);
+            Log.d("ListFiles", "FliesRead: " + fileList()[0]);
+            return customer;
+        } catch (FileNotFoundException f) {
+            f.printStackTrace();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Customer class not found");
+            c.printStackTrace();
+        } return null;
     }
 }
--- app/src/main/java/pt/ulisboa/tecnico/sise/insure/insureapp/calls/Login.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/insure/insureapp/calls/Login.java
@@ -2,9 +2,17 @@ package pt.ulisboa.tecnico.sise.insure.insureapp.calls;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import pt.ulisboa.tecnico.sise.insure.insureapp.GlobalState;
 import pt.ulisboa.tecnico.sise.insure.insureapp.activities.MainActivity;
@@ -14,49 +22,52 @@ public class Login extends AsyncTask <String, Void, Integer> {
 
     private static final String TAG = "Login";
     Context _context;
-    public GlobalState globalState = new GlobalState();
+    String username;
+    String password;
+    Customer customer;
+    private GlobalState _globalState;
 
 
-    public Login(Context context){
+    public Login(Context context, GlobalState globalState){
         _context = context;
+        _globalState = globalState;
     }
 
     protected Integer doInBackground(String... params) {
         //publishProgress("Testing method call login right...");
         Log.d("test", "test");
         try {
-            String username = params[0];
-            String password = params[1];
+            username = params[0];
+            password = params[1];
             Log.d(TAG, username);
             Log.d(TAG, password);
             int sessionId = WSHelper.login(username, password);        // exists and password correct
+            customer = WSHelper.getCustomerInfo(sessionId);
+            customer.setSessionId(sessionId);
             Log.d(TAG, "Login result => " + sessionId);
-            globalState.setSessionId(sessionId);
+            _globalState.setSessionId(sessionId);
             return sessionId;
         } catch (Exception e) {
             Log.d(TAG, e.toString());
-            //publishProgress("failed.\n");
         }
         return null;
     }
 
     @Override
     protected void onPostExecute(Integer sessionId) {
-        Customer customer = null;
         if(sessionId == 0){
             Toast.makeText(_context, "Invalid arguments", Toast.LENGTH_SHORT).show();
         }else if (sessionId > 0){
-            //try {
-            //    OutputStream outputStream;
-            //  outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-            //  ObjectOutputStream out = new ObjectOutputStream(outputStream);
-            //  out.writeObject(customer);
-            //} catch (Exception e) {
-            //  e.printStackTrace();
-            //}
+            try {
+                _globalState.setCustomer(customer);
+                _globalState.writeFile(customer);
+                Log.d(TAG, "fez o write file");
+            } catch (Exception e) {
+                Log.d(TAG, "nao fez o write");
+                Log.d(TAG, e.toString());
+            }
             _context.startActivity(new Intent(_context,MainActivity.class));
+
         }
-
-
     }
 }
